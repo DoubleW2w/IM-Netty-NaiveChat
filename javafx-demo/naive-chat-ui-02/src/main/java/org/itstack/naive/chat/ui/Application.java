efc947755de5f946c8e8a7a4619bcc7fe3c4e5fe
DoubleW2w <1049951363@qq.com>
2024-01-21 03:13:46 +0800

    feat:添加chat-ui-02实现
    1.登录窗体分析：操作栏、头像、用户输入、登录按钮、版本
    2.修改原有项目的错误

---
+++ javafx-demo/naive-chat-ui-02/src/main/java/org/itstack/naive/chat/ui/Application.java
@@ -0,0 +1,22 @@
+package org.itstack.naive.chat.ui;
+
+import org.itstack.naive.chat.ui.view.Login;
+import javafx.stage.Stage;
+
+/**
+ * @author: DoubleW2w
+ * @description:
+ * @date: 2024/1/21 0:56
+ * @project: IM-Netty-NaiveChat
+ */
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Login login = new Login();
+        login.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
